d179fc01e45e49058be9995d47984aff192f5cb0
Mohamed <mohamedmena.mmv@gmail.com>
2026-09-10 16:58:20 +0100

    First commit, and note starting point with React and Component

---
+++ ReactAndComponent.jsx
@@ -0,0 +1,16 @@
+// I) What is React:
+//   React is a JavaScript library for building a web and mobile user interfaces.
+//   It doesn't handle routing or managing complex app logic but it uses other libraries with it to handle other task (Redux, React Router).
+//   It has a component based architecture:
+//      -Breaks UI into small reusable pieces wich can be composed to make more complexe UI.
+
+// II) What is a COmponent:
+//    A component is a piece of the UI that has its own logic and appearance.
+//    It can be as small as a button or as large as an entire page. They can be reusable with the same or different data.
+
+// III) How to wirte it:
+//     A component is just a JavaScript function that returns some HTML describing what should appears on screen (JSX)
+// ex:
+
+//random component:
+function Fn () {}
