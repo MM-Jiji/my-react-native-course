0f89c8f1ea13a2d43de08b77d63ae0437cc50844
Mohamed <mohamedmena.mmv@gmail.com>
2026-09-17 17:54:50 +0100

    context as a solution for prop drilling

---
+++ Course17ContextAnduseApi.jsx
@@ -0,0 +1,10 @@
+// I)Context:
+//   Context lets you share data across your component tree without having to manually pass
+//   props through every intermediatecomponent:
+//   It lets you:
+//     -Lets you broadcast a value at high level.
+//     -Any component nested inside can subscribe to it directly.
+
+// II)use API:
+//    -use is simply a more flexible alternative to useContext.
+//    -It does the same job (reading values from context) but without the restriction that come with hooks.
